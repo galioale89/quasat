47de32eef35ca3458c6ceb7963636db32c6435a6
unknown <vimmus.integracao@gmail.com>
2022-07-24 09:16:41 -0300

    Vimmus 3.0

--- routes/gerenciamento.js
+++ routes/gerenciamento.js
@@ -400,8 +400,8 @@ router.get('/selecao', ehAdmin, (req, res) => {
         }
     ]).then(result => {
         result.map(item => {
-            cliente = item.clientes;
-            console.log(item.clientes);
+            cliente = item.clientes[0].nome;
+            console.log(cliente);
             if (item.status == 'Enviado' && item.ganho == false && naoVazio(item.motivo) == false) {
                 if (item.datacad < parseFloat(datafim) && item.datacad > parseFloat(dataini)) {
                     if (naoVazio(item.valor)) {
