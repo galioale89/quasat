a6c227ebc92da0f1d7195bc94feddd87635f58e5
galioale89 <96298214+galioale89@users.noreply.github.com>
2022-08-09 12:43:57 -0300

    Add files via upload

--- routes/gerenciamento.js
+++ routes/gerenciamento.js
@@ -2468,51 +2468,51 @@ router.get('/fotos/:id', ehAdmin, (req, res) => {
         console.log('projeto=>' + projeto)
         console.log('projeto.cliente=>' + projeto.cliente)
         Cliente.findOne({ _id: projeto.cliente }).lean().then((cliente_projeto) => {
-            
-            // let lista_proposta = projeto.proposta
-            // let lista_doc = []
-            // let lista_local = []
-            // let lista_entrada = []
-            // let lista_disjuntor = []
-            // let lista_trafo = []
-            // let lista_telhado = []
-            // let lista_localizacao = []
-            // let lista_medidor = []
-            // if (naoVazio(projeto.documento)) {
-            //     lista_doc = listaFotos(projeto.documento)
-            // }
-            // // if (naoVazio(projeto.local)) {
-            // //     lista_local = listaFotos(projeto.local)
-            // // }
-            // if (naoVazio(projeto.entrada)) {
-            //     lista_entrada = listaFotos(projeto.entrada)
-            // }               
-            // if (naoVazio(projeto.disjuntor)) {
-            //     lista_disjuntor = listaFotos(projeto.disjuntor)
-            // }                                                
-            // if (naoVazio(projeto.trafo)) {
-            //     lista_trafo = listaFotos(projeto.trafo)
-            // }
-            // if (naoVazio(projeto.telhado_foto)) {
-            //     lista_telhado = listaFotos(projeto.telhado_foto)
-            // }
-            // if (naoVazio(projeto.localizacao)) {
-            //     lista_localizacao = listaFotos(projeto.localizacao)
-            // }
-            // if (naoVazio(projeto.medidor)) {
-            //     lista_medidor = listaFotos(projeto.medidor)
-            // }                        
 
-            // if (funges || funpro) {
-            //     proandges = true
-            // } else {
-            //     proandges = false
+            let lista_proposta = projeto.proposta
+            let lista_doc = []
+            let lista_local = []
+            let lista_entrada = []
+            let lista_disjuntor = []
+            let lista_trafo = []
+            let lista_telhado = []
+            let lista_localizacao = []
+            let lista_medidor = []
+            if (naoVazio(projeto.documento)) {
+                lista_doc = listaFotos(projeto.documento)
+            }
+            // if (naoVazio(projeto.local)) {
+            //     lista_local = listaFotos(projeto.local)
             // }
+            if (naoVazio(projeto.entrada)) {
+                lista_entrada = listaFotos(projeto.entrada)
+            }               
+            if (naoVazio(projeto.disjuntor)) {
+                lista_disjuntor = listaFotos(projeto.disjuntor)
+            }                                                
+            if (naoVazio(projeto.trafo)) {
+                lista_trafo = listaFotos(projeto.trafo)
+            }
+            if (naoVazio(projeto.telhado_foto)) {
+                lista_telhado = listaFotos(projeto.telhado_foto)
+            }
+            if (naoVazio(projeto.localizacao)) {
+                lista_localizacao = listaFotos(projeto.localizacao)
+            }
+            if (naoVazio(projeto.medidor)) {
+                lista_medidor = listaFotos(projeto.medidor)
+            }                        
+
+            if (funges || funpro) {
+                proandges = true
+            } else {
+                proandges = false
+            }
             res.render('principal/fotos', {
-                // vendedor, orcamentista, funges, funpro, ehMaster, proandges, projeto, cliente_projeto,
-                // lista_doc, lista_entrada, lista_disjuntor, lista_trafo, lista_localizacao, lista_telhado, lista_medidor,
-                // seqdoc: lista_doc.length, seqent: lista_entrada.length, seqdis: lista_disjuntor.length, seqmed: lista_medidor.length,
-                // seqtra: lista_trafo.length, seqloc: lista_localizacao.length, seqtel: lista_telhado.length, lista_proposta
+                vendedor, orcamentista, funges, funpro, proandges, projeto, cliente_projeto,
+                lista_doc, lista_entrada, lista_disjuntor, lista_trafo, lista_localizacao, lista_telhado, lista_medidor,
+                seqdoc: lista_doc.length, seqent: lista_entrada.length, seqdis: lista_disjuntor.length, seqmed: lista_medidor.length,
+                seqtra: lista_trafo.length, seqloc: lista_localizacao.length, seqtel: lista_telhado.length, lista_proposta
             })
         }).catch((err) => {
             req.flash('error_msg', 'Não foi possível encontrar o cliente da proposta<fotos>.')
